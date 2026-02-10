832b15dd7064160ab393548f3de45e3a5b24ca03
subhraneel2005 <digitalsubhraneel@gmail.com>
2026-02-11 01:41:34 +0530

    getting h3-index from lat-long

--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
+import h3 from "h3-js"
+
 const server = Bun.serve({
     port: process.env.PORT || 8080,
     routes: {
         "/": () => new Response("hello chads"),
-        "/ping": () => new Response("pong")
+        "/ping": () => new Response("pong"),
+        "/h3-index": () => getH3Index()
     }
 })
+
+function getH3Index() {
+    const h3Index = h3.latLngToCell(23.0301216, 88.5257018, 10)
+
+    return Response.json(h3Index);
+}
 
 console.debug(server.development)
 console.debug(server.hostname)
@@ -23,8 +32,23 @@ const options = {
     maximumAge: 0
 }
 
-function success(pos: GeolocationPosition) {
+function currentLocation(pos: GeolocationPosition) {
+    const crds = pos.coords;
+    console.log("your location");
+    console.log("lat: " + crds.latitude);
+    console.log("long: " + crds.longitude);
+    console.log("accuracy: " + crds.accuracy);
 
+    const resObj = {
+        success: true,
+        locationData: {
+            lat: crds.latitude,
+            long: crds.longitude,
+            accuracy: crds.accuracy
+        }
+    }
+
+    return Response.json(resObj)
 }
 
 
