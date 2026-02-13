c2dce876624c989e54ad5318bead6ed6f8bfb9b0
subhraneel2005 <digitalsubhraneel@gmail.com>
2026-02-13 20:40:58 +0530

    feat: added simple websocket connection logic

--- index.ts
+++ index.ts
@@ -1,55 +1,127 @@
-import h3 from "h3-js"
+// import h3 from "h3-js"
 
-const server = Bun.serve({
-    port: process.env.PORT || 8080,
-    routes: {
-        "/": () => new Response("hello chads"),
-        "/ping": () => new Response("pong"),
-        "/h3-index": () => getH3Index()
+import type { BunRequest } from "bun";
+
+// const server = Bun.serve({
+//     port: process.env.PORT || 8080,
+//     routes: {
+//         "/": () => new Response("hello chads"),
+//         "/ping": () => new Response("pong"),
+//         "/h3-index": () => getH3Index()
+//     }
+// })
+
+// function getH3Index() {
+//     const h3Index = h3.latLngToCell(23.0301216, 88.5257018, 10)
+
+//     return Response.json(h3Index);
+// }
+
+// console.debug(server.development)
+// console.debug(server.hostname)
+// console.debug(server.id)
+// console.debug(server.pendingRequests)
+// console.debug(server.port)
+// console.debug(server.protocol)
+
+// console.log("server running on " + server.url)
+
+// // getCurrentLocation
+
+// const options = {
+//     enableHighAccuracy: true,
+//     timeout: 3000,
+//     maximumAge: 0
+// }
+
+// function currentLocation(pos: GeolocationPosition) {
+//     const crds = pos.coords;
+//     console.log("your location");
+//     console.log("lat: " + crds.latitude);
+//     console.log("long: " + crds.longitude);
+//     console.log("accuracy: " + crds.accuracy);
+
+//     const resObj = {
+//         success: true,
+//         locationData: {
+//             lat: crds.latitude,
+//             long: crds.longitude,
+//             accuracy: crds.accuracy
+//         }
+//     }
+
+//     return Response.json(resObj)
+// }
+
+
+// WEBSOCKETS
+
+type WebSocketsData = {
+    createdAt: number;
+    username: string;
+    channelId: string;
+    userId: string
+}
+
+interface User {
+    userId: string;
+    imageUrl: string;
+    username: string;
+    createdAt: number;
+}
+
+interface Channel {
+    channelId: string;
+    channelName: string;
+    createdAt: number;
+
+}
+
+const server = Bun.serve<WebSocketsData>({
+    port: process.env.PORT,
+
+    fetch(req, server) {
+
+        const url = new URL(req.url);
+
+        if (url.pathname.startsWith("/channel/")) {
+            const id = url.pathname.split("/")[2];
+
+
+
+            const success = server.upgrade(req, {
+                data: {
+                    createdAt: Date.now(),
+                    username: "someRandomShit",
+                    channelId: id!,
+                    userId: "someUUID"
+                }
+            });
+
+            if (success) return undefined;
+            return
+        }
+
+        return new Response("upgrade failed", { status: 500 })
+    },
+    websocket: {
+        message(ws, message) {
+            ws.send(message)
+        },
+        open(ws) {
+            console.log(ws.data.channelId)
+        },
+        close(ws, code, reason) {
+            ws.send(reason)
+        },
+        drain(ws) {
+
+        },
     }
 })
 
-function getH3Index() {
-    const h3Index = h3.latLngToCell(23.0301216, 88.5257018, 10)
+console.debug("server listening on port: " + server.url);
 
-    return Response.json(h3Index);
-}
-
-console.debug(server.development)
-console.debug(server.hostname)
-console.debug(server.id)
-console.debug(server.pendingRequests)
-console.debug(server.port)
-console.debug(server.protocol)
-
-console.log("server running on " + server.url)
-
-// getCurrentLocation
-
-const options = {
-    enableHighAccuracy: true,
-    timeout: 3000,
-    maximumAge: 0
-}
-
-function currentLocation(pos: GeolocationPosition) {
-    const crds = pos.coords;
-    console.log("your location");
-    console.log("lat: " + crds.latitude);
-    console.log("long: " + crds.longitude);
-    console.log("accuracy: " + crds.accuracy);
-
-    const resObj = {
-        success: true,
-        locationData: {
-            lat: crds.latitude,
-            long: crds.longitude,
-            accuracy: crds.accuracy
-        }
-    }
-
-    return Response.json(resObj)
-}
 
 
 
